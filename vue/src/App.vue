b4ef10c02df52998d425d9ed9afd86f5de5df2dd
Frank-Star-fn <2439206073@qq.com>
2024-12-10 16:45:25 +0800

    upd

---
+++ vue/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import BgHeader from './components/BgHeader.vue'
+import MainComponent from './components/MainComponent.vue'
+import '@/assets/style/index_output.css' // 全局引入
+</script>
+
+<template>
+  <main>
+    <BgHeader />
+
+    <MainComponent />
+  </main>
+
+</template>
+
+<style scoped>
+</style>
---
+++ vue/src/components/BgHeader.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <div class="bg-theme-4 h-14.5">
+    </div>
+  </div>
+
+</template>
